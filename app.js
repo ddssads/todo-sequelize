d2653d180c0419e8a96ddc95f4741361cda65cc0
ddssads <tw0937608360@gmail.com>
2021-01-27 00:21:29 +0800

    feat: add login & register page

--- app.js
+++ app.js
@@ -13,9 +13,33 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 //首頁路由
-
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+//登入 註冊 登出路由
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 
