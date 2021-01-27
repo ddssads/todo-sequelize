a953dc2bd0318d72d3f06cdc8567c5abb5d3ee52
ddssads <tw0937608360@gmail.com>
2021-01-27 18:24:33 +0800

    add todo & user model relation

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const methodOverride = require('method-override')
 const app = express()
 const PORT = 3000
 
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -35,7 +39,9 @@ app.get('/users/register', (req, res) => {
 })
 
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
 })
 
 app.get('/users/logout', (req, res) => {
