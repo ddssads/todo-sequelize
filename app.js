25a2b0e4afec040c1855bcfe0abfd338c047e141
ddssads <tw0937608360@gmail.com>
2021-01-28 13:47:05 +0800

    refactor: refactor users routes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const bcrypt = require('bcryptjs')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('./config/passport')
@@ -9,10 +8,6 @@ const routes = require('./routes')
 const app = express()
 const PORT = 3000
 
-const db = require('./models')
-const passport = require('passport')
-const Todo = db.Todo
-const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
@@ -23,58 +18,16 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+
 usePassport(app)
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
   next()
 })
+
 app.use(routes)
-
-
-//登入 註冊 登出路由
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-app.post('/users/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ where: { email } }).then(user => {
-    if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash
-      }))
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
-  })
-})
-
-app.get('/users/logout', (req, res) => {
-  req.logout()
-  res.redirect('/users/login')
-})
 
 
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const { authenticator } = require('../middleware/auth')
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 
 router.use('/todos', authenticator, todos)
-router.use('/', home)
+router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router
