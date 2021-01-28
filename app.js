f8c9b35427a9927daec06339587dc0be24c4615f
ddssads <tw0937608360@gmail.com>
2021-01-28 14:21:22 +0800

    feat: add fb login feature

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
 const session = require('express-session')
-const usePassport = require('./config/passport')
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 usePassport(app)
+
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
