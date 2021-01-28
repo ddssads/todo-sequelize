f039dff64551a5baac5b0e7fb9427833c84be216
ddssads <tw0937608360@gmail.com>
2021-01-28 11:27:48 +0800

    feat: add login feature

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser')
 const bcrypt = require('bcryptjs')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const usePassport = require('./config/passport')
 
 const app = express()
 const PORT = 3000
 
 const db = require('./models')
+const passport = require('passport')
 const Todo = db.Todo
 const User = db.User
 
@@ -16,7 +18,12 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+usePassport(app)
 //首頁路由
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -35,17 +42,14 @@ app.get('/todos/:id', (req, res) => {
 })
 
 //登入 註冊 登出路由
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
