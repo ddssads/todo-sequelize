c5b4ef04973a62f8781f1892f197431b35f329d0
ddssads <tw0937608360@gmail.com>
2021-01-28 13:56:24 +0800

    feat: add flash message

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -13,7 +13,8 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login',
+  failureFlash: true
 }))
 
 router.get('/register', (req, res) => {
@@ -22,10 +23,27 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ messages: '所有欄位都是必填' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ messages: '密碼與確認密碼不相符' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ where: { email } }).then(user => {
     if (user) {
-      console.log('User already exists')
+      errors.push({ messages: '這個email已經註冊過' })
       return res.render('register', {
+        errors,
         name,
         email,
         password,
@@ -47,6 +65,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出')
   res.redirect('/users/login')
 })
 
